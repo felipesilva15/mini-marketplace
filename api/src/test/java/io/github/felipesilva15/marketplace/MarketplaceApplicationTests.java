c2b8883922e39b6f5159ca71feccc2eabda28418
felipe.silva <felipe.silva@pwi.com.br>
2025-07-11 13:35:59 -0300

    feat: Criando fonts base da API

---
+++ api/src/test/java/io/github/felipesilva15/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipesilva15.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
